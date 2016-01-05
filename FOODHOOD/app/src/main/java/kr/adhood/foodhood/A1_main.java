b6f79798a34a09b6fef19a064541ee2d0ca1d1a8
abalon1210 <abalon1210@daum.net>
2016-01-05 11:20:30 +0900

    A1 layout & S3 ing

--- FOODHOOD/app/src/main/java/kr/adhood/foodhood/A1_main.java
+++ FOODHOOD/app/src/main/java/kr/adhood/foodhood/A1_main.java
@@ -1,9 +1,17 @@
 package kr.adhood.foodhood;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by Administrator on 2015-12-29.
  */
-public class A1_main {
+public class A1_main extends Activity {
+
+    protected void onCrete(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a1_main);
 
 
+    }
 }
